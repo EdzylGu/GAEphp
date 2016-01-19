b756d23f4c904f13ad9e43b593c88498869c7a9d
Edzyl Gu <anedzyl@gmail.com>
2016-01-19 19:13:25 +0800

    GAE
    Bulid a web page with facebook Page Plugin on GAE.

---
+++ index.php
@@ -0,0 +1,167 @@
+<?php?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>NDHU X Facebook Page</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link type="text/css" rel="css" href="/css/skel-noscript.css" />
+			<link type="text/css" rel="css" href="/css/style.css" />
+			<link type="text/css" rel="css" href="/css/style-desktop.css" />
+		</noscript>
+	</head>
+	<body class="homepage">
+	
+	<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.3&appId=243182775806167";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		
+		
+
+	<div id="header-wrapper">
+
+		<!-- Header -->
+			<div id="header">
+				<div class="container">
+						
+					<!-- Logo -->
+						<div id="logo">
+							<h1><a href="index.php">NDHU X Facebook Page</a></h1>
+						</div>
+					
+					
+				</div>
+			</div>
+		
+			<div id="banner">
+				<div class="container">
+					<section>
+						<span class="fa fa-thumbs-up"></span>
+						<header>
+							<h2>Recommend Facebook Pages</h2>
+							<span class="byline">推薦與東華大學相關的臉書專頁</span>
+						</header>
+					</section>	
+				</div>
+			</div>
+		<!-- /Banner -->
+
+	</div>
+
+	
+	<!-- Main -->
+		<div id="main">
+		
+			<div class="container">
+			
+				<section>
+					<header>
+						<h2>學生社群</h2>
+						
+					</header>
+				</section>
+
+				<div class="row">
+					<section class="6u right">
+						<div class="fb-page" data-href="https://www.facebook.com/pages/TEDx-NDHU/625838434195042?__mref=message_bubble" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/TEDx-NDHU/625838434195042?__mref=message_bubble"><a href="https://www.facebook.com/pages/TEDx-NDHU/625838434195042?__mref=message_bubble">TEDx NDHU</a></blockquote></div></div>
+	
+						<p>TEDxNDHU係由東華大學學生自主性成立的「TEDx Project」。並通過TED大會官方核准設立辦理年會演講活動。
+							我們希望以建立東華學生對校園認同感做為出發點，藉由TEDx的網路串流，使得TEDxNDHU成為分享故事的平台，散播草根故事、引進國際觀點！讓花蓮不再只是被台灣邊緣化的鄉鎮。</p>
+					</section>				
+					<section class="6u left">
+						<div class="fb-page" data-href="https://www.facebook.com/ndhucourse?fref=ts" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/ndhucourse?fref=ts"><a href="https://www.facebook.com/ndhucourse?fref=ts">Tewkr 推課東華</a></blockquote></div></div>
+						<p>Tewkr-您可以在網站上大方地分享推薦的課程，也可以查看其他同學們對於課程的看法，來評估是否適合該課程。<br/>
+							進入網站，您可以分享對於課程的看法，並給予該課程一個GPA等第。您也可以查看其他同學們對於課程的看法，來評估您是否適合該課程。</p>
+					</section>
+					
+				</div>
+				<div class="row">
+					<section class="6u right">
+						<div class="fb-page" data-href="https://www.facebook.com/pages/&#x9ec3;&#x5927;&#x5e25;&#x7684;&#x6771;&#x65b9;&#x5c0f;&#x57ce;/506992916123937?fref=pb&amp;hc_location=profile_browser" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/&#x9ec3;&#x5927;&#x5e25;&#x7684;&#x6771;&#x65b9;&#x5c0f;&#x57ce;/506992916123937?fref=pb&amp;hc_location=profile_browser"><a href="https://www.facebook.com/pages/&#x9ec3;&#x5927;&#x5e25;&#x7684;&#x6771;&#x65b9;&#x5c0f;&#x57ce;/506992916123937?fref=pb&amp;hc_location=profile_browser">黃大帥的東方小城</a></blockquote></div></div>
+
+						<p>哈囉，我是大帥，一個充滿熱血彭派的旅遊達人，陽光活力的微笑是我的招牌，在這個我的小小世界裡，有各種好吃的美食，好玩的地點，超讚的住宿等，吃喝玩樂食衣住行樣樣來，秉持著走到哪玩到哪，哪裡好玩哪裡去，哪裡有趣衝到底，哪裡好吃撐肚皮，看著我的日誌，讓你們遊遍花蓮，跟著我一起上山下海展開一段驚奇卻又闔家玩樂的旅程！</p>
+					</section>				
+					<section class="6u left">
+						<div class="fb-page" data-href="https://www.facebook.com/hatendhu?fref=pb&amp;hc_location=profile_browser" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/hatendhu?fref=pb&amp;hc_location=profile_browser"><a href="https://www.facebook.com/hatendhu?fref=pb&amp;hc_location=profile_browser">黑特東華 Hatendhu  【原靠北東華】</a></blockquote></div></div>
+						<p>【本專頁為參考其他黑特與靠北版成立，規則及改進事項近期內將公告，發文者皆視為已讀規則】，用HATE都知道這要怎麼用。</p>
+					</section>
+					
+				</div>
+				<div class="row">
+					<section class="6u right">
+						<div class="fb-page" data-href="https://www.facebook.com/NDHUTM" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/NDHUTM"><a href="https://www.facebook.com/NDHUTM">NDHU ToastMasters Int. 國立東華大學國際英語演講社</a></blockquote></div></div>
+						<p>「國際演講會」是個國際性非營利教育協會，主張提升自我「溝通」與「領導」能力，若你／妳跟我們一樣，尋一個持續而溫馨的全英語環境，我們歡迎您的加入！</p>
+						</section>				
+					<section class="6u left">
+						<div class="fb-page" data-href="https://www.facebook.com/NDHUAmbassador" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/NDHUAmbassador"><a href="https://www.facebook.com/NDHUAmbassador">國立東華大學親善大使團 NDHU Friendship Ambassador</a></blockquote></div></div>
+						<p>東華大學親善大使團是隸屬於學務處 課外活動組，
+本著熱心服務、友愛校園中每個人的精神為努力的目標，
+以笑容面對全校的師生以及校外的貴賓，
+主持、接待、頒獎、彩妝、綁髮、校園導覽等等...都是我們的基礎訓練，
+希望未來能夠創造更好的親善，
+更友善的東華校園!</p></section>
+					
+				</div>
+				<br/><hr><br/>
+				<section>
+					<header>
+						<h2>好咖啡</h2>
+						<span class="byline">包含東華附近以及花蓮市區附近的咖啡廳</span>
+					</header>
+				</section>
+
+				<div class="row">
+					<section class="6u right">
+						<div class="fb-page" data-href="https://www.facebook.com/LeFlaneur.cafe?ref=ts&amp;fref=ts" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/LeFlaneur.cafe?ref=ts&amp;fref=ts"><a href="https://www.facebook.com/LeFlaneur.cafe?ref=ts&amp;fref=ts">Le Flâneur</a></blockquote></div></div>
+						</section>				
+					<section class="6u left">
+						<div class="fb-page" data-href="https://www.facebook.com/SevenBeansCoffeeShop?fref=pb&amp;hc_location=profile_browser" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/SevenBeansCoffeeShop?fref=pb&amp;hc_location=profile_browser"><a href="https://www.facebook.com/SevenBeansCoffeeShop?fref=pb&amp;hc_location=profile_browser">Seven Beans 咖啡舍</a></blockquote></div></div>
+						</section>
+					
+				</div>
+				<div class="row">
+					<section class="6u right">
+						<div class="fb-page" data-href="https://www.facebook.com/9803cafe?fref=ts" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/9803cafe?fref=ts"><a href="https://www.facebook.com/9803cafe?fref=ts">9803咖啡館</a></blockquote></div></div>
+						</section>				
+					<section class="6u left">
+						<div class="fb-page" data-href="https://www.facebook.com/OOPS.TAIWAN?fref=ts" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/OOPS.TAIWAN?fref=ts"><a href="https://www.facebook.com/OOPS.TAIWAN?fref=ts">Oops&quot;驚奇咖啡</a></blockquote></div></div>
+						</section>
+					
+				</div>
+					
+			</div>
+		</div>
+	<!-- /Main -->
+	<!-- Footer -->
+	
+			<div id="banner" style="padding: 1em 0em 1em 0em;">
+				<div class="container">
+					<section>
+						
+						<header>
+							<li>Copyright &copy; 2015 </li>
+						</header>
+					</section>	
+				</div>
+			</div>
+		
+
+	</body>
+</html>
+
+		
+		
+	
